3ab59df2a48d99caf45c47fe38f554abd8bd9544
Tobias Gurtzick <magic@wizardtales.com>
2016-01-27 23:46:30 +0100

    basic example

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+  * This project should give an insight in how a project might utilize the
+  * programable API.
+  *
+  * This assumes we have a simple web project, which should initialize the
+  * database by itself. There are further examples on how to control and use the
+  * API in the cli project. This is the straightforward usage of db-migrate.
+  *
+  * You may also find a better integrated version of this with custom configs
+  * and other supported features in the server.js example.
+  */
+
+// first require the package
+var DBMigrate = require( 'db-migrate' );
+
+// The next step is to get a new instance of DBMigrate
+var dbmigrate = DBMigrate.getInstance(true);
